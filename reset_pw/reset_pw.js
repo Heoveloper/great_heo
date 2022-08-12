87d6b2dee5149a70c60404e722befc7d3f74d280
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-12 17:57:05 +0900

    modify & add

---
+++ reset_pw/reset_pw.js
@@ -0,0 +1,7 @@
+function reset_pw_complete() {
+    const url = "reset_pw_complete.html";
+    const name = "비밀번호 재설정 완료";
+    const option = "width = 500, height = 500, top = 330, left = 660, location = no"
+
+    window.open(url, name, option);
+}
