f45d499fceef1a097beaf7c0af9ef955ccc9511a
Junhyeok Heo <wnsgur5508@gmail.com>
2022-08-14 23:10:58 +0900

    +- delay

--- reset_pw_2/reset_pw_2.js
+++ reset_pw_2/reset_pw_2.js
@@ -5,5 +5,9 @@ function close() {
     document.querySelector(".background").className = "background";
 }
 
+// setTimeout(function delay() {
+//     location.replace('/main/main.html');
+// }, 2000);
+
 document.querySelector(".confirm-btn").addEventListener('click', show);
 document.querySelector(".confirm-btn").addEventListener('click', close);
