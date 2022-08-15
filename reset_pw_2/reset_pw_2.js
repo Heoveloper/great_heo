64da05d9931ab2140f5400eaa978baac7862224b
Junhyeok Heo <wnsgur5508@gmail.com>
2022-08-15 19:28:53 +0900

    mod link

--- reset_pw_2/reset_pw_2.js
+++ reset_pw_2/reset_pw_2.js
@@ -1,5 +1,5 @@
 function reset_pw_2_complete() {
-    const url = "reset_pw_complete.html";
+    const url = "reset_pw_2_complete.html";
     const name = "비밀번호 재설정 완료";
     const option = "width = 500, height = 300, top = 300, left = 700, location = no";
 
